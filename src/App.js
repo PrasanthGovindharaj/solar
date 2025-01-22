3cb13dafe5dd71c92e3ec7d8c478cf5e3cbed515
PrasanthGovindharaj <prasanthjg2@gmail.com>
2025-01-22 14:44:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Solar from './components/Solar';
+
+function App() {
+  return (
+    <div>
+      <main class="solar-system">
+      <Solar/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
